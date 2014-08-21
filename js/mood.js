4c56137a71f9387880646f68da17ff7d25cca47b
Rohit Khanduri <rohit.khanduri@hotmail.com>
2014-08-21 09:28:57 +0530

    Autocomplete
    Added autocomplete location selector in moods.html using google APIs

--- js/mood.js
+++ js/mood.js
@@ -13,7 +13,12 @@ $(document).ready(function()
         
         uiChanges();
         
-        
+        var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});
+		google.maps.event.addListener(autocomplete, 'place_changed', function()
+		{
+			var place = autocomplete.getPlace();
+			console.log(place.address_components);
+		});
       
         
         $(".activitiesButton").click(function()
