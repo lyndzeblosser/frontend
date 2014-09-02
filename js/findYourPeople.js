a19588b5a40bd558559e425006d000c9e724d8fd
Rohit Khanduri <rohit.khanduri@hotmail.com>
2014-09-02 23:23:37 +0530

    Round 14
    Changes for Find Your People page.

--- js/findYourPeople.js
+++ js/findYourPeople.js
@@ -3,18 +3,23 @@ var jsonImgObj = [];
 
 $(document).ready(function () 
                 {
-                    var parameters = location.search;
+    				
+					var parameters = location.search;
                     var parameter = parameters.split("?");
                     console.log("getParameterByName: radius: " + getParameterByName('radius')); 
                     getResult(getParameterByName('latitude'), getParameterByName('longitude'), getParameterByName('topics'), getParameterByName('radius'), getParameterByName('activity'), getParameterByName('userid'));
                    
                 });
 
-function fillImgDetails(data)
+function fillImgDetails(data, img)
 {
-    for(var i=0;i<data.length;i++)
+    var useridArrayInitial = img.split("/");
+	var useridArray = useridArrayInitial[useridArrayInitial-1].split(".jpeg");
+	var userid = useridArray[0];
+	
+	for(var i=0;i<data.length;i++)
     {
-        if(data[i].userid === api.getField("title"))
+        if(data[i].userid === userid)
         {
             $("#name").val(data[i].name);
             $("#age").val(data[i].age);  
@@ -47,23 +52,7 @@ function getResult(latitude, longitude, selectedTopics, radius, activity, userid
             dataType: "json",
             success: function (data) 
             {
-                console.log("data: " + data);
-                if(data != "undefined")
-                {    
-                    for(var i=0;i<data.length;i++)
-                    {
-                        imgItem["image"] = 'img/' + data[i].userid + '.jpeg';
-                        imgItem["title"] = data[i].userid;
-                        
-                        jsonImgObj.push(imgItem);
-                        
-                        //console.log("selected images json array: " + jsonImgObj);
-                        
-                    }
-                    
-                }
-                
-                
+              
             },
             error: function (error, message) 
             {
@@ -71,92 +60,64 @@ function getResult(latitude, longitude, selectedTopics, radius, activity, userid
             },
             complete: function(data)
             {
-                
-                $.supersized({
-				
-					// Functionality
-					slideshow               :   1,			// Slideshow on/off
-					autoplay				:	0,			// Slideshow starts playing automatically
-					start_slide             :   1,			// Start slide (0 is random)
-					stop_loop				:	0,			// Pauses slideshow on last slide
-					random					: 	0,			// Randomize slide order (Ignores start slide)
-					slide_interval          :   3000,		// Length between transitions
-					transition              :   6, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-					transition_speed		:	1000,		// Speed of transition
-					new_window				:	1,			// Image links open in new window/tab
-					pause_hover             :   0,			// Pause slideshow on hover
-					keyboard_nav            :   1,			// Keyboard navigation on/off
-					performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-					image_protect			:	1,			// Disables image dragging and right click with Javascript
-															   
-					// Size & Position						   
-					min_width		        :   0,			// Min width allowed (in pixels)
-					min_height		        :   0,			// Min height allowed (in pixels)
-					vertical_center         :   1,			// Vertically center background
-					horizontal_center       :   1,			// Horizontally center background
-					fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
-					fit_portrait         	:   1,			// Portrait images will not exceed browser height
-					fit_landscape			:   0,			// Landscape images will not exceed browser width
-															   
-					// Components							
-					slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-					thumb_links				:	0,			// Individual thumb links for each slide
-					thumbnail_navigation    :   0,			// Thumbnail navigation
-					slides 					:  	jsonImgObj,
-												
-					// Theme Options			   
-					progress_bar			:	1,			// Timer for each slide							
-					mouse_scrub				:	0
-					
-				});
-                
-                fillImgDetails(data);
-                
-                $("#inviteButton").click(function()
+                console.log("data: " + data);
+                if(data != "undefined")
+                {    
+                    for(var i=0;i<data.length;i++)
                     {
-
-                        var slideTitle = api.getField('title');
-	                    console.log("slide title: " + slideTitle);
+                        imgItem["image"] = '../img/' + data[i].userid + '.jpeg';
+                        imgItem["title"] = data[i].userid;
                         
-                        if(selectedUsers === [])
-                        {
-                            selectedUsers[0] = slideTitle;   
-                        }
-                        else
-                        {
-                            selectedUsers.push(slideTitle);
-                        }
+                        jsonImgObj.push(imgItem);
+                        
+                    }
+                    
+                }
+				
+				/*function to change images and toggle information about user*/
+                var imgList = jsonImgObj;
+				var imgJSON = JSON.parse(imgList);
+				var clickCount = 0;
+				
+				var showInfo = document.getElementById( 'showInfo' ),
+				menuBottom = document.getElementById( 'cbp-spmenu-s4' ),
+				changeSlide = document.getElementById( 'changeSlide' ),
+				body = $("#body");
+			
+				var initialImg = JSON.parse(JSON.stringify(imgList[clickCount])).src.toString();
+				console.log("src-----" + img);
+				
+				/*Setting initial image*/
+				body.css({"backgroundImage": "url("+initialImg+")"});
+				console.log("clickCount: " + clickCount);
+				console.log("image being set as background: " + initialImg);
 
-                        console.log('selectedUsers: ' + selectedUsers);
-                    });
-                
-                $("#prevslide").click(function(data)
-                      {
-                          var slideTitle = api.getField('title');
-                          for(var i=0;i<data.length;i++)
-                          {
-                              if(data[i].userid === slideTitle)
-                              {
-                                $("#name").val(data[i].name);
-                                $("#age").val(data[i].age);  
-                                $("#bio").val(data[i].bio);  
-                              }
-                          }
-                      });
-                
-                $("#nextslide").click(function(data)
-                      {
-                          var slideTitle = api.getField('title');
-                          for(var i=0;i<data.length;i++)
-                          {
-                              if(data[i].userid === slideTitle)
-                              {
-                                $("#name").val(data[i].name);
-                                $("#age").val(data[i].age);  
-                                $("#bio").val(data[i].bio);  
-                              }
-                          }
-                      });
+				showInfo.onclick = function() {
+					classie.toggle( this, 'active' );
+					classie.toggle( menuBottom, 'cbp-spmenu-open' );
+				};
+				
+				changeSlide.onclick = function() {
+				
+					console.log("length of imgJSON : " + imgList.length);
+					
+					if(clickCount == 0)
+					{
+						clickCount = 1;
+					}
+					if(clickCount >= imgList.length)
+					{
+						clickCount = 0;
+					}
+					
+					var img = JSON.parse(JSON.stringify(imgList[clickCount])).src.toString();
+					
+					console.log("clickCount: " + clickCount);
+					console.log("image being set as background: " + img);
+					body.css({"backgroundImage": "url(" + img + ")"});
+					clickCount++;
+					fillImgDetails(data, img);
+				};
             }
         });
     
