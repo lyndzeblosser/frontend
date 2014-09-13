cefc0e5176b4b0abbeb602d30729b70043faa361
sulagna <sulagna.das@anarkeysolutions.com>
2014-09-13 19:00:28 +0530

    fixed icons -partial browser resizing

--- js/mood.js
+++ js/mood.js
@@ -23,15 +23,15 @@ function activitiesImages(){
         if(this.getAttribute("selected")==0){
             $(".activitiesImages").each(function(){
                 this.setAttribute("selected",0);
-                this.firstChild.setAttribute("src","images/"+this.getAttribute("activity")+"invert.png")
+                this.firstChild.setAttribute("src","images/"+this.getAttribute("activity")+"invert.svg")
                 $(this).css("background","#ffffff")
             })
-            this.firstChild.setAttribute("src","images/"+this.getAttribute("activity")+".png") 
+            this.firstChild.setAttribute("src","images/"+this.getAttribute("activity")+".svg") 
             $(this).css("background","#cc0000")
             this.setAttribute("selected", 1)
         }else{
             this.setAttribute("selected",0);
-            this.firstChild.setAttribute("src","images/"+this.getAttribute("activity")+"invert.png")
+            this.firstChild.setAttribute("src","images/"+this.getAttribute("activity")+"invert.svg")
             $(this).css("background","#ffffff")
         }
         
